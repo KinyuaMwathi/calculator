087f51a86bec462b341616a154ab667f435f0675
Charles Mwathi <mwathi.kinyua@gmail.com>
2021-04-13 14:46:12 +0300

    Add user interface logic for 3 additional maths operators

--- js/scripts.js
+++ js/scripts.js
@@ -23,4 +23,31 @@ $(document).ready(function() {
 	
 		$("#output").text(result);
 	});
+
+	$("form#subtract").submit(function(event) {
+		event.preventDefault();
+		const number1 = parseInt($("#subtract1").val());
+		const number2 = parseInt($("#subtract2").val());
+		const result = subtract(number1, number2);
+
+		$("#output").text(result);
+	});
+
+	$("form#multiply").submit(function(event) {
+		event.preventDefault();
+		const number1 = parseInt($("#multiply1").val());
+		const number2 = parseInt($("#multiply2").val());
+		const result = multiply(number1, number2);
+
+		$("#output").text(result);
+	});
+
+	$("form#divide").submit(function(event) {
+		event.preventDefault();
+		const number1 = parseInt($("#divide1").val());
+		const number2 = parseInt($("#divide2").val());
+		const result = multiply(number1, number2);
+
+		$("#output").text(result);
+	});
 });
